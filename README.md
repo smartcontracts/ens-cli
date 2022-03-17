2d898c39b9b3b91a398b076db5d4ca081f7bdfe4
Kelvin Fichter <kelvin@optimism.io>
2022-03-16 22:21:52 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ens-cli
+
+This package only does one thing.
+
+## Installation
+
+```
+npm install -g ens-cli
+```
+
+## Usage
+
+```
+ens resolve <name>
+```
---
+++ bin/cli.js
@@ -0,0 +1,18 @@
+const { program } = require('commander')
+const { ethers } = require('ethers')
+
+program
+  .name('ens')
+  .description('CLI for interacting with the Ethereum Name Service')
+  .version('1.0.0')
+
+program
+  .command('resolve <name>')
+  .description('Resolve an ENS name to an address')
+  .action(async (name) => {
+    const provider = new ethers.providers.InfuraProvider()
+    const address = await provider.resolveName(name)
+    console.log(address)
+  })
+
+program.parse()
